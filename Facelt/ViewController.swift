d3dcc0bc671a5c8ff366f006484ca8db0054c272
LeePepe <1105208297@qq.com>
2016-10-31 15:05:44 +0800

    Initial Commit

---
+++ Facelt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facelt
+//
+//  Created by 李天培 on 2016/10/31.
+//  Copyright © 2016年 lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

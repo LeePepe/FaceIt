4354fc8aa719823b099deb68810547defa9e0867
LeePepe <1105208297@qq.com>
2016-12-22 11:22:46 +0800

    add animation to FaceIt

---
+++ Facelt/EyeView.swift
@@ -0,0 +1,21 @@
+//
+//  EyeView.swift
+//  Facelt
+//
+//  Created by 李天培 on 2016/12/21.
+//  Copyright © 2016年 lee. All rights reserved.
+//
+
+import UIKit
+
+class EyeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
